7e1fdc3ec5c567c934ae5ab2f9ef0b9e357cfe98
fengwei <feng.wei@trs.com.cn>
2014-04-30 13:39:46 +0800

    trshybase source use nGram watermark

--- trshybase-source/src/main/java/com/trs/smas/flume/TRSHybaseSource.java
+++ trshybase-source/src/main/java/com/trs/smas/flume/TRSHybaseSource.java
@@ -8,9 +8,7 @@ package com.trs.smas.flume;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +16,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.time.DateUtils;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
@@ -51,13 +48,16 @@ import com.trs.hybase.client.params.SearchParams;
  * .from = 2014/04/25 13:30:00
  * .delay = -10
  * </code>
+ * 
  * @since huangshengbo @ Apr 16, 2014 6:04:13 PM
- *
+ * 
  */
-public class TRSHybaseSource extends AbstractSource implements PollableSource, Configurable {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(TRSHybaseSource.class);
-	
+public class TRSHybaseSource extends AbstractSource implements PollableSource,
+		Configurable {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(TRSHybaseSource.class);
+
 	private String url;
 	private String username;
 	private String password;
@@ -67,20 +67,22 @@ public class TRSHybaseSource extends AbstractSource implements PollableSource, C
 	private List<String> bodyArgs;
 	private String[] headers;
 	private String watermarkField;
-//	private String identifierField;
+	private int ngram;
 	private String from;
-	private int delay;
 	private Path checkpoint;
-	
+
 	private int batchSize;
-	
+
 	private TRSConnection connection;
-	private OffsetWatermark watermark;
-	
+	private NGramOffsetWatermark watermark;
+
 	private SourceCounter sourceCounter;
-	
-	/* (non-Javadoc)
-	 * @see org.apache.flume.conf.Configurable#configure(org.apache.flume.Context)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.flume.conf.Configurable#configure(org.apache.flume.Context)
 	 */
 	public void configure(Context context) {
 		url = context.getString("url");
@@ -89,10 +91,10 @@ public class TRSHybaseSource extends AbstractSource implements PollableSource, C
 		database = context.getString("database");
 		filter = context.getString("filter");
 		watermarkField = context.getString("watermark");
-//		identifierField = context.getString("identifier");
+		ngram = context.getInteger("ngram");
 		from = context.getString("from");
-		delay = context.getInteger("delay", -10);
-		checkpoint = FileSystems.getDefault().getPath(context.getString("checkpoint"));
+		checkpoint = FileSystems.getDefault().getPath(
+				context.getString("checkpoint"));
 		body = context.getString("body");
 		bodyArgs = new ArrayList<String>();
 		Pattern pattern = Pattern.compile("\\{(.*?)\\}");
@@ -103,34 +105,37 @@ public class TRSHybaseSource extends AbstractSource implements PollableSource, C
 		for (String arg : bodyArgs) {
 			body = body.replace("{" + arg + "}", "%s");
 		}
-		
-		if(!StringUtils.isEmpty( context.getString("headers") )){
+
+		if (!StringUtils.isEmpty(context.getString("headers"))) {
 			headers = context.getString("headers").split(";");
-		}else{
+		} else {
 			headers = new String[0];
 		}
-		
+
 		batchSize = context.getInteger("batchSize", 1000);
-		
-		if(sourceCounter == null){
+
+		if (sourceCounter == null) {
 			sourceCounter = new SourceCounter(getName());
 		}
 	}
 
 	@Override
 	public synchronized void start() {
-		//初始化watermark
+		// 初始化watermark
 		try {
-			watermark = OffsetWatermark.loadFrom(checkpoint);
+			watermark = NGramOffsetWatermark.loadFrom(checkpoint);
 		} catch (IOException e) {
 			LOG.error("Unable to load watermark from" + checkpoint, e);
-			throw new RuntimeException("watermark loading failed, you can delete "+ checkpoint + " and then restart.", e);
+			throw new RuntimeException(
+					"watermark loading failed, you can delete " + checkpoint
+							+ " and then restart.", e);
 		}
-		
-		if(watermark == null){
-			watermark = new OffsetWatermark(watermarkField, from);
+
+		if (watermark == null) {
+			watermark = new NGramOffsetWatermark(watermarkField, from, ngram);
 		}
-		connection = new TRSConnection(this.url, this.username, this.password, new ConnectParams());
+		connection = new TRSConnection(this.url, this.username, this.password,
+				new ConnectParams());
 		sourceCounter.start();
 		super.start();
 	}
@@ -142,73 +147,83 @@ public class TRSHybaseSource extends AbstractSource implements PollableSource, C
 		} catch (TRSException e) {
 			LOG.warn("closing hybase connection failed.", e);
 		}
-		//保存watermark
+		// 保存watermark
 		try {
 			watermark.saveTo(checkpoint);
 		} catch (IOException e) {
-			LOG.error("Unable to save watermark "+ watermark +" to " + checkpoint, e);
+			LOG.error("Unable to save watermark " + watermark + " to "
+					+ checkpoint, e);
 		}
 		super.stop();
 		sourceCounter.stop();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.flume.PollableSource#process()
 	 */
 	public Status process() throws EventDeliveryException {
-		Date cursor = null;
-		try {
-			cursor = DateUtils.parseDate(watermark.getCursor(), new String[]{"yyyy/MM/dd HH:mm:ss"});
-		} catch (ParseException e) {
-			LOG.error("parse watermark cursor error! ", e);
-		}
-		
 		Status status = Status.READY;
-		if(cursor != null && cursor.before(DateUtils.addMinutes(new Date(), delay))){
-			List<Event> buffer = new ArrayList<Event>(batchSize);
-			String query = StringUtils.isEmpty(watermark.getCursor()) ? filter : watermark.getApplyTo() + ": [\"" + watermark.getCursor() + "\" TO *}" + ( StringUtils.isEmpty(filter)? "" : " AND " + filter);
-			
-			TRSResultSet resultSet = null;
-			try {
-				resultSet = connection.executeSelect(this.database, query, watermark.getOffset(), batchSize, new SearchParams().setSortMethod("+" + watermark.getApplyTo()));			
-			} catch (TRSException e) {
-				LOG.error("fail to select "+database+" by "+query,e);
-				return Status.BACKOFF;
-			}
-			if(resultSet.size() == 0){
-				resultSet.close();
-				return Status.BACKOFF;
-			}
-			for (int i = 0; i < Math.min(batchSize, resultSet.size()); i++) {
-				resultSet.moveNext();
-				try {
-					TRSRecord record = resultSet.get();
-					
-					List<String> values = new ArrayList<String>(this.bodyArgs.size());
-					
-					for(String field : this.bodyArgs){
-						String value = record.getString(field);
-						values.add(StringUtils.defaultString(StringUtils.startsWith(value, "@") ? "//" + value : value));
-					}
-					Map<String,String> header = new HashMap<String,String>(this.headers.length);
-					for(String key : this.headers){
-						header.put(key, record.getString(key));
-					}
-					buffer.add(EventBuilder.withBody(String.format(body, values.toArray()).getBytes(),header));
-					watermark.rise(record.getString(watermark.getApplyTo()));
-				} catch (TRSException e) {
-					LOG.error("can not read data from resultset "+watermark,e);
-					break;
-				}
-			}
-			
-			LOG.debug("{} record(s) ingested. current watermark:{}",resultSet.size(), watermark);
-			getChannelProcessor().processEventBatch(buffer);
-			sourceCounter.incrementAppendBatchAcceptedCount();
-			sourceCounter.addToEventAcceptedCount(buffer.size());
-			resultSet.close();
+
+		List<Event> buffer = new ArrayList<Event>(batchSize);
+		String query = StringUtils.isEmpty(watermark.getCursor()) ? filter
+				: watermark.getApplyTo() + ": [\"" + watermark.getCursor()
+						+ "\" TO *}"
+						+ (StringUtils.isEmpty(filter) ? "" : " AND " + filter);
+
+		TRSResultSet resultSet = null;
+		try {
+			resultSet = connection.executeSelect(
+					this.database,
+					query,
+					watermark.getOffset(),
+					batchSize,
+					new SearchParams().setSortMethod("+"
+							+ watermark.getApplyTo()));
+		} catch (TRSException e) {
+			LOG.error("fail to select " + database + " by " + query, e);
+			return Status.BACKOFF;
 		}
-		
+		if (resultSet.size() == 0) {
+			resultSet.close();
+			return Status.BACKOFF;
+		}
+		for (int i = 0; i < Math.min(batchSize, resultSet.size()); i++) {
+			resultSet.moveNext();
+			try {
+				TRSRecord record = resultSet.get();
+
+				List<String> values = new ArrayList<String>(
+						this.bodyArgs.size());
+
+				for (String field : this.bodyArgs) {
+					String value = record.getString(field);
+					values.add(StringUtils.defaultString(StringUtils
+							.startsWith(value, "@") ? "//" + value : value));
+				}
+				Map<String, String> header = new HashMap<String, String>(
+						this.headers.length);
+				for (String key : this.headers) {
+					header.put(key, record.getString(key));
+				}
+				buffer.add(EventBuilder.withBody(
+						String.format(body, values.toArray()).getBytes(),
+						header));
+				watermark.rise(record.getString(watermark.getApplyTo()));
+			} catch (TRSException e) {
+				LOG.error("can not read data from resultset " + watermark, e);
+				break;
+			}
+		}
+
+		LOG.debug("{} record(s) ingested. current watermark:{}",
+				resultSet.size(), watermark);
+		getChannelProcessor().processEventBatch(buffer);
+		sourceCounter.incrementAppendBatchAcceptedCount();
+		sourceCounter.addToEventAcceptedCount(buffer.size());
+		resultSet.close();
+
 		return status;
 	}
 
